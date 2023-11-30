29ee638413082f5fd0453e46f57b696aa4cf6ffa
Praveenreddy <praveen@Praveenreddys-MacBook-Pro.local>
2023-11-30 13:08:29 +0530

    cloud microservices

---
+++ microservices-ws/config-server/config-client-1/src/main/java/com/cts/configclient/controller/TestRestResource.java
@@ -0,0 +1,22 @@
+package com.cts.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+@RefreshScope
+public class TestRestResource {
+	
+	@Value("${name}")
+	private String personName;
+	
+	@GetMapping
+	public String getName() {
+		return "Person Name is "+personName;
+	}
+
+}
